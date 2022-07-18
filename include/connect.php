804fb4750f73838d1b6177ca6214fefb4cae9f14
Vinita Parasrampuria <vp2238@nyu.edu>
2022-07-18 19:31:42 -0400

    initial commit

---
+++ include/connect.php
@@ -0,0 +1,10 @@
+<?php
+    $username = "root";
+    $password = "";
+    $hostname = "localhost";
+    $port = 3307;
+    $db = "myhmsdb";
+    $con = mysqli_connect($hostname, $username, $password, $db, $port);
+    $response = array();
+
+?>
